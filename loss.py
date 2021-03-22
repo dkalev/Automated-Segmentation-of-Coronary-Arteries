f834d8a3e1b1680cce45a4ef4586500675ad1879
Damyan Kalev <dzkalev@gmail.com>
2021-03-22 19:01:38 +0100

    Added combined loss

--- loss.py
+++ loss.py
@@ -16,3 +16,12 @@ class DiceLoss(nn.Module):
         intersection = (pred * targ).sum()
         dice_score = (2. * intersection + self.smooth) / ( pred.sum() + targ.sum() + self.smooth )
         return 1. - dice_score
+
+class DiceBCELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.dice = DiceLoss()
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def forward(self, pred, targ):
+        return self.dice(pred, targ) + self.bce(pred, targ)
