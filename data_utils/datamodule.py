93028d23790f4869b5b0e964afab6fc29046adeb
Damyan Kalev <dzkalev@gmail.com>
2021-04-26 22:26:51 +0200

    Moved to numpy dataset

--- data_utils/datamodule.py
+++ data_utils/datamodule.py
@@ -1,12 +1,11 @@
-import h5py
+import shutil
 import logging
-import traceback
 import numpy as np
 from pathlib import Path
 from torch.utils.data import DataLoader
 from pytorch_lightning import LightningDataModule
 
-from .dataset import AsocaNumpyDataset, AsocaDataset
+from .dataset import AsocaDataset, AsocaDataset
 from .dataset_builder import DatasetBuilder
 from .sampler import ASOCASampler
 
@@ -23,11 +22,11 @@ class AsocaDataModule(DatasetBuilder, LightningDataModule):
                 batch_size=1,
                 patch_size=32,
                 patch_stride=None,
-                normalize='global', # has no effect on resample; only when building from scratch
+                normalize=False,
                 data_clip_range=(0, 400),
                 sourcepath='dataset/ASOCA2020Data.zip',
                 datapath='dataset/asoca.hdf5',
-                output_dir='dataset', **kwargs):
+                data_dir='dataset/processed', **kwargs):
         super().__init__(logger, *args, **kwargs)
         self.batch_size = batch_size
 
@@ -43,46 +42,48 @@ class AsocaDataModule(DatasetBuilder, LightningDataModule):
 
         self.normalize = normalize
         self.sourcepath = sourcepath
-        self.output_dir = output_dir
+        self.data_dir = data_dir
         self.datapath = Path(datapath)
 
     def prepare_data(self):
-        action = self.verify_dataset()
+        if self.is_valid():
+            logger.info(f'Using existing dataset located at {self.data_dir}')
+            return
 
-        # if action == 'resample':
-        #     with h5py.File(self.datapath, 'r+') as f:
-        #         try:
-        #             self.resample_patches(f)
-        #         except Exception:
-        #             if 'temp' in f: del f['temp']
-        #             logger.error(traceback.format_exc())
-        # elif action == 'build':
-        #     subdirs = ['Train', 'Train_Masks', 'Test']
-        #     folders_exist = [ Path(self.output_dir, subdir).is_dir() for subdir in subdirs ]
-        #     if not all(folders_exist):
-        #         logger.info(f'Extracting data from {self.sourcepath}')
-        #         self.extract_files(subdirs)
+        if Path(self.data_dir).is_dir():
+            self.logger.info(f'Found corrupted dataset. Building from scratch.')
+            shutil.rmtree(self.data_dir)
 
-        #     logger.info('Building HDF5 dataset')
-        #     volume_path = Path(self.output_dir, 'Train')
-        #     mask_path = Path(self.output_dir, 'Train_Masks')
-        #     self.build_dataset(volume_path, mask_path, output_dir=Path(self.output_dir, 'processed'))
+        subdirs = ['Train', 'Train_Masks', 'Test']
+        folders_exist = [ Path(self.data_dir, subdir).is_dir() for subdir in subdirs ]
+        if not all(folders_exist):
+            logger.info(f'Extracting data from {self.sourcepath}')
+            self.extract_files(subdirs)
+
+        logger.info('Building dataset')
+        volume_path = Path(self.data_dir, 'Train')
+        mask_path = Path(self.data_dir, 'Train_Masks')
+        self.build_dataset(volume_path, mask_path)
+
+        for subdir in subdirs:
+            shutil.rmtree(Path(self.data_dir, subdir))
 
         logger.info('Done')
-
+    
     def train_dataloader(self, batch_size=None):
         if batch_size is None: batch_size = self.batch_size
-        train_split = AsocaNumpyDataset(split='train')
+        train_split = AsocaDataset(split='train')
         sampler = ASOCASampler(train_split.vol_meta, shuffle=True)
         return DataLoader(train_split, sampler=sampler, batch_size=batch_size, num_workers=12, pin_memory=True)
 
     def val_dataloader(self, batch_size=None):
         if batch_size is None: batch_size = self.batch_size
-        valid_split = AsocaNumpyDataset(split='valid')
+        valid_split = AsocaDataset(split='valid')
         sampler = ASOCASampler(valid_split.vol_meta)
         return DataLoader(valid_split, sampler=sampler, batch_size=batch_size, num_workers=12, pin_memory=True)
 
     def volume_dataloader(self, file_id, batch_size=None):
+        # FIXME update to work with new dataset
         if batch_size is None: batch_size = self.batch_size
         ds = AsocaDataset(self.datapath, file_id=file_id)
         meta = {
--- data_utils/dataset.py
+++ data_utils/dataset.py
@@ -1,59 +1,11 @@
-import ast
 import json
-import h5py
 from pathlib import Path
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+
 class AsocaDataset(Dataset):
-    def __init__(self, ds_path, file_id=None, split='train'):
-        self.ds_path = ds_path
-        ds = h5py.File(ds_path, 'r')
-        if file_id is not None:
-            vol_meta = ast.literal_eval(ds.attrs['vol_shapes'])
-            for split in ['train', 'valid']: # FIXME same variable as kwarg
-                if file_id in vol_meta[split]:
-                    self.split = split
-                    break
-            if not hasattr(self, 'split'): #FIXME there's gotta be a better way
-                raise ValueError(f'File id: {file_id} could not be found in the volumes meta data')
-
-            self.shape_orig = vol_meta[self.split][file_id]['shape_orig']
-            self.shape_patched = vol_meta[self.split][file_id]['shape_patched']
-            self.start_idx = vol_meta[self.split][file_id]['start_idx']
-            self.len = vol_meta[self.split][file_id]['n_patches']
-        else:
-            self.start_idx = 0
-            self.split = split
-            self.len = len(ds[split]['volumes'])
-        del ds
-
-    def add_offset(self, index):
-        if isinstance(index, int):
-            index += self.start_idx
-        elif isinstance(index, slice):
-            start = index.start + self.start_idx if index.start is not None else None
-            stop = index.stop + self.start_idx if index.stop is not None else None
-            index = slice(start, stop, index.step)
-        else:
-            raise ValueError(f'Unsupported index type: {type(index)}')
-        return index
-
-    def __len__(self):
-        return self.len
-
-    def __getitem__(self, index):
-        self.ds = h5py.File(self.ds_path, 'r')[self.split]
-        index = self.add_offset(index)
-        x, y = self.ds['volumes'][index], self.ds['masks'][index]
-        x, y = torch.tensor(x), torch.tensor(y)
-        if len(x.shape) == 3:
-            x, y = x.unsqueeze(0), y.unsqueeze(0)
-        return x, y
-
-
-class AsocaNumpyDataset(Dataset):
     def __init__(self, ds_path='dataset/processed', split='train'):
         self.ds_path = Path(ds_path, split)
         with open(Path(ds_path, 'dataset.json'), 'r') as f:
@@ -73,6 +25,18 @@ class AsocaNumpyDataset(Dataset):
             file_id = index.start // self.max_patches
             idx = slice(index.start % self.max_patches, index.stop % self.max_patches)
         return file_id, idx
+    
+    def cur_vol(self, file_id):
+        if not hasattr(self, '_cur_vol') or self._cur_vol_id != file_id:
+            self._cur_vol = np.load(Path(self.ds_path, 'vols', f'{file_id}.npy'))
+            self._cur_vol_id = file_id
+        return self._cur_vol
+
+    def cur_mask(self, file_id):
+        if not hasattr(self, '_cur_mask') or self._cur_mask_id != file_id:
+            self._cur_mask = np.load(Path(self.ds_path, 'masks', f'{file_id}.npy'))
+            self._cur_mask_id = file_id
+        return self._cur_mask
 
     def __getitem__(self, index):
         file_id, idx = self.split_index(index)
