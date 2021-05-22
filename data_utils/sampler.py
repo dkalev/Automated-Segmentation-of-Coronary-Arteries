3c2b9fcbc85d8c38bd7b85ddcf04c2caaf138586
Damyan Kalev <dzkalev@gmail.com>
2021-05-22 15:27:42 +0200

    Removed useless parameter in sampler

--- data_utils/sampler.py
+++ data_utils/sampler.py
@@ -17,7 +17,6 @@ class ASOCASampler(Sampler):
         self.oversample = oversample
         self.binary_weights = binary_weights
         self.perc_per_epoch = perc_per_epoch
-        self.oversample_coef = oversample_coef
         self.alpha = alpha
 
     @property
@@ -25,12 +24,12 @@ class ASOCASampler(Sampler):
         if not hasattr(self, '_max_patches'):
             self._max_patches = max([ v['n_patches'] for v in self.shapes.values() ])
         return self._max_patches
-    
+
     @property
     def total_patches(self):
         if not hasattr(self, '_total_patches'):
             self._total_patches = sum([ int(v['n_patches'] * self.perc_per_epoch) for v in self.shapes.values() ])
-        return self._total_patches 
+        return self._total_patches
 
     def get_sample_weights(self, samples):
         if self.binary_weights:
@@ -43,16 +42,15 @@ class ASOCASampler(Sampler):
             return p
         else:
             samples = np.array(samples)
-            samples = samples * self.oversample_coef + samples.max()
             return samples / np.sum(samples)
-    
+
     def get_file_ids(self):
         file_ids = list(self.shapes.keys())
         if self.shuffle:
             return self.gen.permutation(file_ids)
         else:
             return file_ids
-    
+
     def get_patch_idxs(self, file_id):
         meta = self.shapes[file_id]
         n_patches = meta['n_patches']
@@ -68,6 +66,7 @@ class ASOCASampler(Sampler):
         for file_id in self.get_file_ids():
             for index in self.get_patch_idxs(file_id):
                 yield file_id * self.max_patches + index
-                
+
     def __len__(self):
         return self.total_patches
+
